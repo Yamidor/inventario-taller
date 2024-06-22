2a93d622d8974eccb024bc1c9f9df07f7497fb51
Yamidor <41703136+Yamidor@users.noreply.github.com>
2024-06-22 13:14:24 -0500

    Update page.jsx desbloquear tipo de producto completo

--- src/app/articulos/page.jsx
+++ src/app/articulos/page.jsx
@@ -57,13 +57,9 @@ const Articulos = () => {
           <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
             <div>
               <label for="tipo" class="block text-gray-700">Tipo</label>
-              <select id="tipo" name="tipo" class="w-full px-3 py-2 border rounded"  disabled>
-                <option value="Producto" ${
-                  articulo && articulo.tipo === "Producto" ? "selected" : ""
-                }>Producto</option>
-                <option value="Mano de obra" ${
-                  articulo && articulo.tipo === "Mano de obra" ? "selected" : ""
-                }>Mano de obra</option>
+              <select id="tipo" name="tipo" class="w-full px-3 py-2 border rounded" >
+                <option value="Producto">Producto</option>
+                <option value="Mano de obra">Mano de obra</option>
               </select>
             </div>
             <div>
